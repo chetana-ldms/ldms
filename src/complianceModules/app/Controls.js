96644dc1500af4107613f48d7656b2452e3fab6b
chetana <chetna.sangamad@sollishealth.com>
2024-02-14 01:05:01 +0530

    Monitoring page UI

--- src/complianceModules/app/Controls.js
+++ src/complianceModules/app/Controls.js
@@ -1,58 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Controle = () => {
-  const [activeTab, setActiveTab] = useState('inScope');
+  const [activeTab, setActiveTab] = useState("inScope");
 
   return (
-    <div className='compliance-controls'>
+    <div className="compliance-controls">
       <h2>Controls In Scope</h2>
-      <div className='row'>
-        <div className='col-lg-12'>
-          <div className='float-right'>
-            <button className='btn btn-primary btn-new btn-small'>Create New Control</button>
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="float-right">
+            <button className="btn btn-primary btn-new btn-small">
+              Create New Control
+            </button>
           </div>
         </div>
       </div>
       <br />
       <hr />
-      <div className='row'>
-        <div className='col-lg-3 controls-left'>
+      <div className="row">
+        <div className="col-lg-3 controls-left">
           <h4>All Controls</h4>
-          <br />
-          <p>Frameworks</p>
+          <hr />
+          <h4>Frameworks</h4>
+          <ul>
+            <li>
+              <i className="fa fa-compass" />
+              CCM
+            </li>
+            <li>
+              <i className="fa fa-compass" />
+              CCPA
+            </li>
+            <li>
+              <i className="fa fa-compass" />
+              CMMC
+            </li>
+            <li>
+              <i className="fa fa-compass" />
+              COBIT
+            </li>
+          </ul>
         </div>
-        <div className='col-lg-9'>
-          <div className='sort-section row'>
-            <div className='col-lg-3 d-flex '>
-              <ul className='nav nav-tabs p-0 border-0 fs-8'>
-                <li className='nav-item'>
+        <div className="col-lg-9">
+          <div className="sort-section row">
+            <div className="col-lg-3 d-flex ">
+              <ul className="nav nav-tabs p-0 border-0 fs-8">
+                <li className="nav-item">
                   <a
-                    className={`nav-link ${activeTab === 'inScope' ? 'active' : ''}`}
-                    onClick={() => setActiveTab('inScope')}
+                    className={`nav-link ${
+                      activeTab === "inScope" ? "active" : ""
+                    }`}
+                    onClick={() => setActiveTab("inScope")}
                   >
                     In Scope
                   </a>
                 </li>
-                <li className='nav-item'>
+                <li className="nav-item">
                   <a
-                    className={`nav-link ${activeTab === 'outScope' ? 'active' : ''}`}
-                    onClick={() => setActiveTab('outScope')}
+                    className={`nav-link ${
+                      activeTab === "outScope" ? "active" : ""
+                    }`}
+                    onClick={() => setActiveTab("outScope")}
                   >
                     Out of Scope
                   </a>
                 </li>
               </ul>
             </div>
-            <div className='col-lg-6'>
+            <div className="col-lg-6">
               <input
-                type='text'
-                placeholder='Search controls by name, description, code'
-                style={{ width: '100%' }}
+                type="text"
+                placeholder="Search controls by name, description, code"
+                style={{ width: "100%" }}
               />
             </div>
-            <div className='col-lg-3'>
-              Sort By:
-              <select>
+            <div className="col-lg-3">
+              <select placeholder="Sort By:">
+                <option>Sort By:</option>
                 <option>Name</option>
                 <option>Control code</option>
                 <option>All controls</option>
@@ -62,40 +86,40 @@ const Controle = () => {
             </div>
           </div>
           <br />
-          <div className='controls-content'>
-            {activeTab === 'inScope' && (
-              <div className='tab-pane fade show active' id='inScope'>
-                <table className='table'>
+          <div className="controls-content">
+            {activeTab === "inScope" && (
+              <div className="tab-pane fade show active" id="inScope">
+                <table className="table">
                   <tbody>
                     <tr>
                       <td>
-                        <div className='checkbox'>
-                          <input type='checkbox' />
-                          <i className='fa fa-check-square' />
+                        <div className="checkbox">
+                          <input type="checkbox" />
+                          <i className="fa fa-check-square" />
                         </div>
                       </td>
                       <td>Description</td>
                       <td>
-                        <div className='float-right right-icons'>
-                          <i className='fa fa-paperclip' />
-                          <i className='fa fa-indent' />
-                          <i className='fa fa-users' />
+                        <div className="float-right right-icons">
+                          <i className="fa fa-paperclip" />
+                          <i className="fa fa-indent" />
+                          <i className="fa fa-users" />
                         </div>
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <div className='checkbox'>
-                          <input type='checkbox' />
-                          <i className='fa fa-check-square' />
+                        <div className="checkbox">
+                          <input type="checkbox" />
+                          <i className="fa fa-check-square" />
                         </div>
                       </td>
                       <td>Description</td>
                       <td>
-                        <div className='float-right right-icons'>
-                          <i className='fa fa-paperclip' />
-                          <i className='fa fa-indent' />
-                          <i className='fa fa-users' />
+                        <div className="float-right right-icons">
+                          <i className="fa fa-paperclip" />
+                          <i className="fa fa-indent" />
+                          <i className="fa fa-users" />
                         </div>
                       </td>
                     </tr>
@@ -103,39 +127,39 @@ const Controle = () => {
                 </table>
               </div>
             )}
-            {activeTab === 'outScope' && (
-              <div className='tab-pane fade show active' id='outScope'>
-                <table className='table'>
+            {activeTab === "outScope" && (
+              <div className="tab-pane fade show active" id="outScope">
+                <table className="table">
                   <tbody>
                     <tr>
                       <td>
-                        <div className='checkbox'>
-                          <input type='checkbox' />
-                          <i className='fa fa-check-square' />
+                        <div className="checkbox">
+                          <input type="checkbox" />
+                          <i className="fa fa-check-square" />
                         </div>
                       </td>
                       <td>Description Change</td>
                       <td>
-                        <div className='float-right right-icons'>
-                          <i className='fa fa-paperclip' />
-                          <i className='fa fa-indent' />
-                          <i className='fa fa-users' />
+                        <div className="float-right right-icons">
+                          <i className="fa fa-paperclip" />
+                          <i className="fa fa-indent" />
+                          <i className="fa fa-users" />
                         </div>
                       </td>
                     </tr>
                     <tr>
                       <td>
-                        <div className='checkbox'>
-                          <input type='checkbox' />
-                          <i className='fa fa-check-square' />
+                        <div className="checkbox">
+                          <input type="checkbox" />
+                          <i className="fa fa-check-square" />
                         </div>
                       </td>
                       <td>Description Change</td>
                       <td>
-                        <div className='float-right right-icons'>
-                          <i className='fa fa-paperclip' />
-                          <i className='fa fa-indent' />
-                          <i className='fa fa-users' />
+                        <div className="float-right right-icons">
+                          <i className="fa fa-paperclip" />
+                          <i className="fa fa-indent" />
+                          <i className="fa fa-users" />
                         </div>
                       </td>
                     </tr>
--- src/complianceModules/app/Monitoring.js
+++ src/complianceModules/app/Monitoring.js
@@ -1,9 +1,162 @@
-import React from 'react'
+import React from "react";
 
 function Monitoring() {
   return (
-    <div>Monitoring</div>
-  )
+    <div className="compliance compliance-monitor compliance-controls">
+      <h2>Monitoring</h2>
+      <br />
+      <div className="row monitor-card">
+        <div className="col-lg-4">
+          <div className="card text-center alert-warning">
+            <h4>of Tests Passed</h4>
+            <p>
+              <i className="fa fa-percent green" /> 42%
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="card text-center alert-danger">
+            <h4>Failed Tests</h4>
+            <p>
+              <i className="fa fa-minus red" /> 42%
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="card text-center alert-success">
+            <h4>Passed Tests</h4>
+            <p>
+              <i className="fa fa-plus green" /> 35%
+            </p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <hr />
+      <br />
+      <div className="row">
+        <div className="col-lg-3 controls-left">
+          {/* <h4>All Tests</h4> */}
+          {/* <hr /> */}
+          <h4>Test Results</h4>
+          <ul>
+            <li>
+              <i className="fa fa-minus-circle" />
+              Failed
+            </li>
+            <li>
+              <i className="fa fa-plus-circle" />
+              Passed
+            </li>
+            <li>
+              <i className="fa fa-exclamation-circle" />
+              Error
+            </li>
+            <hr />
+            <h4>Category</h4>
+            <li>
+              <i className="fa fa-compass" />
+              Policy
+            </li>
+          </ul>
+        </div>
+        <div className="col-lg-9">
+          <div className="sort-section row">
+            <div className="col-lg-11">
+              <input
+                type="text"
+                placeholder="Search automated tests..."
+                style={{ width: "100%" }}
+              />
+            </div>
+            <div className="col-lg-1">
+              <i className="fa fa-download green" />
+            </div>
+          </div>
+          <br />
+          <div className="controls-content">
+            <div className="tab-pane fade show active" id="inScope">
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <td>
+                      <h4>MFA on Identity Provider</h4>
+                    </td>
+                    <td>
+                      <span className="gray">
+                        Last Tested: 11 hours ago <i className="fa fa-users" />
+                      </span>
+                    </td>
+                    <td>
+                      <div className="float-right right-icons">
+                        <button className="btn btn-danger btn-small">
+                          <i className="fa fa-exclamation-circle" /> Fix Now
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h4>Password Manager Required</h4>
+                    </td>
+                    <td>
+                      <span className="gray">
+                        Last Tested: 11 hours ago{" "}
+                        <i className="fa fa-file-text" />
+                      </span>
+                    </td>
+                    <td>
+                      <div className="float-right right-icons">
+                        <button className="btn btn-danger btn-small">
+                          <i className="fa fa-exclamation-circle" /> Fix Now
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h4>Has a Backup Policy</h4>
+                    </td>
+                    <td>
+                      <span className="gray">
+                        Last Tested: 11 hours ago{" "}
+                        <i className="fa fa-file-text" />
+                      </span>
+                    </td>
+                    <td>
+                      <div className="float-right right-icons">
+                        <button className="btn btn-danger btn-small">
+                          <i className="fa fa-exclamation-circle" /> Fix Now
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <h4>Daily Database Backup</h4>
+                    </td>
+                    <td>
+                      <span className="gray">
+                        Last Tested: 11 hours ago{" "}
+                        <i className="fa fa-file-text" />
+                      </span>
+                    </td>
+                    <td>
+                      <div className="float-right right-icons">
+                        <button className="btn btn-danger btn-small">
+                          <i className="fa fa-exclamation-circle" /> Fix Now
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Monitoring
+export default Monitoring;
