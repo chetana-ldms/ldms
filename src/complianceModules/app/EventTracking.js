3d085e9a92acf54543dc621b4bb994a3bb595d11
NaveenaEB <Naveena.EB@lancesoft.in>
2024-02-20 17:28:15 +0530

    changes

--- src/complianceModules/app/EventTracking.js
+++ src/complianceModules/app/EventTracking.js
@@ -1,28 +1,72 @@
-import { React, useState } from "react";
-import { Modal, Button } from "react-bootstrap";
-import EventTrackingPopUp from "./EventTrackingPopUp";
+import React, {useState} from 'react'
+import {Modal} from 'react-bootstrap'
+import EventTrackingPopUp from './EventTrackingPopUp'
 
 function EventTracking() {
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false)
+
+  const eventData = [
+    {
+      id: 1,
+      user: 'Arunachalam R',
+      timestamp: 'Feb 14, 2024 @9:30:31 AM',
+      category: 'Evidence',
+      connection: '-',
+      result: '-',
+      type: 'Evidence Created',
+      description: 'Arunachalam created new evidence',
+    },
+    {
+      id: 2,
+      user: 'Chetana S',
+      timestamp: 'Nov 20, 2024 @7:20:05 AM',
+      category: 'Company',
+      connection: '-',
+      result: '-',
+      type: 'Company Roles Updated',
+      description: 'Chetana granted/revoked role access to users within the company.',
+    },
+    {
+      id: 3,
+      user: 'Arunachalam R',
+      timestamp: 'Feb 14, 2024 @9:30:31 AM',
+      category: 'Evidence',
+      connection: '-',
+      result: '-',
+      type: 'Evidence Created',
+      description: 'Arunachalam created new evidence',
+    },
+    {
+      id: 4,
+      user: 'Chetana S',
+      timestamp: 'Nov 20, 2024 @7:20:05 AM',
+      category: 'Company',
+      connection: '-',
+      result: '-',
+      type: 'Company Roles Updated',
+      description: 'Chetana granted/revoked role access to users within the company.',
+    },
+  ]
 
   const handleRowClick = (item) => {
-    setShowModal(true);
-  };
+    setShowModal(true)
+  }
+
   return (
-    <div className="compliance">
+    <div className='compliance'>
       <h2>Events</h2>
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="compliance-controls">
+      <div className='row'>
+        <div className='col-lg-12'>
+          <div className='compliance-controls'>
             <form>
               <select>
                 <option>All Categories</option>
               </select>
-              {"   "}
+              {'   '}
               <select>
                 <option>All Types</option>
               </select>
-              {"  "}
+              {'  '}
               <select>
                 <option>All Users</option>
               </select>
@@ -31,12 +75,12 @@ function EventTracking() {
         </div>
       </div>
       <hr />
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="table">
-            <table className="table">
+      <div className='row'>
+        <div className='col-lg-12'>
+          <div className='table'>
+            <table className='table'>
               <thead>
-                <tr className="bold">
+                <tr className='bold'>
                   <th>User</th>
                   <th>Timestamp</th>
                   <th>Category</th>
@@ -47,62 +91,28 @@ function EventTracking() {
                 </tr>
               </thead>
               <tbody>
-                <tr onClick={() => handleRowClick()}>
-                  <td>
-                    <span className="initial">AR</span> Arunachalam R
-                  </td>
-                  <td>Feb 14, 2024 @9:30:31 AM</td>
-                  <td>Evidence</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>Evidence Created</td>
-                  <td>
-                    Arunachalam created new evidence
-                    <i className="fa fa-chevron-right float-right" />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="initial">CS</span> Chetana S
-                  </td>
-                  <td>Nov 20, 2024 @7:20:05 AM</td>
-                  <td>Company</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>Company Roles Updated</td>
-                  <td>
-                    Chetana granted/revoked role access to users within the
-                    company. <i className="fa fa-chevron-right float-right" />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="initial">AR</span> Arunachalam R
-                  </td>
-                  <td>Feb 14, 2024 @9:30:31 AM</td>
-                  <td>Evidence</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>Evidence Created</td>
-                  <td>
-                    Arunachalam created new evidence
-                    <i className="fa fa-chevron-right float-right" />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <span className="initial">CS</span> Chetana S
-                  </td>
-                  <td>Nov 20, 2024 @7:20:05 AM</td>
-                  <td>Company</td>
-                  <td>-</td>
-                  <td>-</td>
-                  <td>Company Roles Updated</td>
-                  <td>
-                    Chetana granted/revoked role access to users within the
-                    company. <i className="fa fa-chevron-right float-right" />
-                  </td>
-                </tr>
+                {eventData.map((event) => (
+                  <tr key={event.id} onClick={() => handleRowClick(event)}>
+                    <td>
+                      <span className='initial'>
+                        {event.user
+                          .split(' ')
+                          .map((name) => name.charAt(0))
+                          .join('')}
+                      </span>{' '}
+                      {event.user}
+                    </td>
+                    <td>{event.timestamp}</td>
+                    <td>{event.category}</td>
+                    <td>{event.connection}</td>
+                    <td>{event.result}</td>
+                    <td>{event.type}</td>
+                    <td>
+                      {event.description}
+                      <i className='fa fa-chevron-right float-right' />
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
             <EventTrackingPopUp showModal={showModal} setShowModal={setShowModal} />
@@ -110,7 +120,7 @@ function EventTracking() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default EventTracking;
+export default EventTracking
