cfa454806d789547fad929f0cbc31d3d92fbc9bd
chetana <chetna.sangamad@sollishealth.com>
2024-02-20 13:38:51 +0530

    Event tracking page UI modal

--- src/complianceModules/app/EventTracking.js
+++ src/complianceModules/app/EventTracking.js
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
 function EventTracking() {
   const [showModal, setShowModal] = useState(false);
@@ -46,7 +47,9 @@ function EventTracking() {
               </thead>
               <tbody>
                 <tr onClick={() => handleRowClick()}>
-                  <td>Arunachalam</td>
+                  <td>
+                    <span className="initial">AR</span> Arunachalam R
+                  </td>
                   <td>Feb 14, 2024 @9:30:31 AM</td>
                   <td>Evidence</td>
                   <td>-</td>
@@ -58,7 +61,9 @@ function EventTracking() {
                   </td>
                 </tr>
                 <tr>
-                  <td>Chetana</td>
+                  <td>
+                    <span className="initial">CS</span> Chetana S
+                  </td>
                   <td>Nov 20, 2024 @7:20:05 AM</td>
                   <td>Company</td>
                   <td>-</td>
@@ -70,7 +75,9 @@ function EventTracking() {
                   </td>
                 </tr>
                 <tr>
-                  <td>Arunachalam</td>
+                  <td>
+                    <span className="initial">AR</span> Arunachalam R
+                  </td>
                   <td>Feb 14, 2024 @9:30:31 AM</td>
                   <td>Evidence</td>
                   <td>-</td>
@@ -82,7 +89,9 @@ function EventTracking() {
                   </td>
                 </tr>
                 <tr>
-                  <td>Chetana</td>
+                  <td>
+                    <span className="initial">CS</span> Chetana S
+                  </td>
                   <td>Nov 20, 2024 @7:20:05 AM</td>
                   <td>Company</td>
                   <td>-</td>
@@ -95,6 +104,78 @@ function EventTracking() {
                 </tr>
               </tbody>
             </table>
+            <Modal
+              className="modal"
+              show={showModal}
+              onHide={() => setShowModal(false)}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  <h2>Audit Event</h2>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h4>Event Details</h4>
+                <div>
+                  <span className="initial">AR</span> Arunachalam
+                  <span>arunachalam@lancesoft.com</span>
+                </div>
+                <br />
+                <div>
+                  <p className="bold no-margin">Timestamp:</p>
+                  <p>Feb 14, 2024 @9:30:31 AM</p>
+                </div>
+                <br />
+                <div>
+                  <p className="bold no-margin">Description:</p>
+                  <p>Arunachalam created new evidence.</p>
+                </div>
+                <br />
+                <div>
+                  <p className="bold no-margin">Response:</p>
+                  <p className="link">Show raw evidence</p>
+                  <div className="card pad-10 mg-btm-10">
+                    <div className="row">
+                      <div className="col-lg-8">
+                        <p className="bold no-margin">
+                          Raw Evidence and Event Details .pdf
+                        </p>
+                        <p className="no-margin">
+                          Get a .pdf file that contains raw evidence along with
+                          event details
+                        </p>
+                      </div>
+                      <div className="col-lg-4">
+                        <button className="btn btn-border btn-small float-right">
+                          <i className="fa fa-download link" /> Download
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card pad-10">
+                    <div className="row">
+                      <div className="col-lg-8">
+                        <p className="bold no-margin">Raw Evidence .txt</p>
+                        <p className="no-margin">
+                          Get a .txt file that only shows raw evidence
+                        </p>
+                      </div>
+                      <div className="col-lg-4">
+                        <button className="btn btn-border btn-small float-right">
+                          <i className="fa fa-download link" /> Download
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <br />
+                <div>
+                  <h4>Notes</h4>
+                  <p className="no-margin gray">Add a new message</p>
+                  <textarea className="form-textarea"></textarea>
+                </div>
+              </Modal.Body>
+            </Modal>
           </div>
         </div>
       </div>
