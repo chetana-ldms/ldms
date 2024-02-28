fa0fa5db96febe1bd06079306a80107c273626a2
NaveenaEB <Naveena.EB@lancesoft.in>
2024-02-28 15:03:40 +0530

    task changes

--- src/_metronic/layout/components/header/Navbar.tsx
+++ src/_metronic/layout/components/header/Navbar.tsx
@@ -1,32 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx'
-import {KTSVG, toAbsoluteUrl} from '../../../helpers'
-import {HeaderNotificationsMenu, HeaderUserMenu, Search, ThemeModeSwitcher} from '../../../partials'
-import {useLayout} from '../../core'
+import { KTSVG, toAbsoluteUrl } from '../../../helpers'
+import { HeaderNotificationsMenu, HeaderUserMenu, Search, ThemeModeSwitcher } from '../../../partials'
+import { useLayout } from '../../core'
 import { Link, useNavigate } from 'react-router-dom'
 import { Dropdown, Button } from 'react-bootstrap';
+import { fetchTasksUrl } from '../../../../app/api/TasksApi';
 
 const itemClass = 'ms-1 ms-lg-3'
-const btnClass =
-  'btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px'
+const btnClass = 'btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px w-md-40px h-md-40px'
 const userAvatarClass = 'symbol-35px symbol-md-40px'
 const btnIconClass = 'svg-icon-1'
 
 const Navbar = () => {
-  const {config} = useLayout()
+  const { config } = useLayout()
   const navigate = useNavigate()
 
   const handleLogout = () => {
     navigate('/auth');
   };
   const userName = sessionStorage.getItem('userName');
+  const [tasks, setTasks] = useState<any[]>([]); 
 
+  const ownerUserId = Number(sessionStorage.getItem('userId'))
+
+  const reload = async () => {
+    try {
+      const data = await fetchTasksUrl(ownerUserId)
+      setTasks(data || []); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    reload()
+  }, [])
+  const handleNotification = () =>{
+    navigate(`/qradar/tasks/list`)
+  }
+  
   return (
     <div className='app-navbar flex-shrink-0'>
       <p className='d-flex m-5'>Welcome &nbsp;<b>{" "} { userName}!</b></p>
-        {/* <button className="btn btn-primary m-3" onClick={handleLogout} >LogOut </button> */}
-
-
       <div className={clsx('app-navbar-item', itemClass)}>
         <HeaderNotificationsMenu />
       </div>
@@ -47,13 +63,15 @@ const Navbar = () => {
         <Dropdown>
           <Dropdown.Toggle as={Button} variant="link" id="dropdown-basic" className='bell'>
             <i className='fa fa-bell link'/>
-            {" "}<span className='count'>1</span>
+            {" "}<span className='count'>{tasks.length}</span>
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Notification 1</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Notification 2</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Notification 3</Dropdown.Item>
+            {tasks.map(task => (
+               <Dropdown.Item onClick={handleNotification}>
+                 {task.taskType}
+               </Dropdown.Item>
+            ))}
           </Dropdown.Menu>
         </Dropdown>
       </div>
@@ -62,4 +80,4 @@ const Navbar = () => {
   )
 }
 
-export {Navbar}
+export { Navbar }
