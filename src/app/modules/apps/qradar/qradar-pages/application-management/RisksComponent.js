af582bc4258978f3d11035cfaa8678fc631439a9
NaveenaEB <Naveena.EB@lancesoft.in>
2024-03-05 14:24:37 +0530

    changes

--- src/app/modules/apps/qradar/qradar-pages/application-management/RisksComponent.js
+++ src/app/modules/apps/qradar/qradar-pages/application-management/RisksComponent.js
@@ -1,42 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react'
+import {fetchApplicationsAndRisksUrl} from '../../../../../api/ApplicationSectionApi'
+import { getCurrentTimeZone } from '../../../../../../utils/helper'
+import { UsersListLoading } from '../components/loading/UsersListLoading';
 
 function RisksComponent() {
-  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [risk, setRisk] = useState([])
+  console.log(risk, 'risk')
+  const orgId = Number(sessionStorage.getItem('orgId'))
 
   const fetchData = async () => {
-    try {
-      const response = await fetch('API_URL');
-      const jsonData = await response.json();
-      setData(jsonData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
+    const data = {
+      orgID: orgId,
     }
-  };
+    try {
+      setLoading(true);
+      const response = await fetchApplicationsAndRisksUrl(data)
+      setRisk(response)
+    } catch (error) {
+      console.error( error)
+    }finally{
+      setLoading(false);
+    }
+  }
   useEffect(() => {
-    fetchData();
-  }, []); 
+    fetchData()
+  }, [])
 
   return (
     <div>
-      <h2>RisksComponent</h2>
-      <table>
+      <table className="table align-middle gs-0 gy-4 dash-table alert-table">
         <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
+        <tr className="fw-bold text-muted bg-blue">
+            <th className="min-w-50px fs-12">Application Name</th>
+            <th className="min-w-50px fs-12">Type </th>
+            <th className="min-w-50px fs-12">Versions </th>
+            <th className="min-w-50px fs-12">Vender </th>
+            <th className="min-w-50px fs-12"> Highest Severity </th>
+            <th className="min-w-50px fs-12">Highest Vulnerability score </th>
+            <th className="min-w-50px fs-12">Highest NVD base score </th>
+            <th className="min-w-50px fs-12">Most common Status </th>
+            <th className="min-w-50px fs-12">Exploited in the Wild </th>
+            <th className="min-w-50px fs-12">Exploited Maturity </th>
+            <th className="min-w-50px fs-12">Remediation Level </th>
+            <th className="min-w-50px fs-12">Number of CVEs</th>
+            <th className="min-w-50px fs-12">Number of Endpoints </th>
+            <th className="min-w-50px fs-12">Application Detection Date </th>
+            <th className="min-w-50px fs-12">Date from Detection </th>
           </tr>
         </thead>
         <tbody>
-          {data.map(item => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
+        {loading && <UsersListLoading />}
+          {risk.map((item) => (
+            <tr>
               <td>{item.name}</td>
+              <td>{item.applicationType}</td>
+              <td>{item.versionCount}</td>
+              <td>{item.vendor}</td>
+              <td>{item.highestSeverity}</td>
+              <td></td>
+              <td>{item.highestNvdBaseScore ?? 0}</td>
+              <td>{item.statuses.find(status => status.count === Math.max(...item.statuses.map(status => status.count))).label}</td>
+              <td></td>
+              <td></td>
+              <td>{item.remediationLevel ?? 0}</td>
+              <td>{item.cveCount}</td>
+              <td>{item.endpointCount}</td>
+              <td>{getCurrentTimeZone(item.detectionDate)}</td>
+              <td></td>
+           
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  );
+  )
 }
 
-export default RisksComponent;
+export default RisksComponent
