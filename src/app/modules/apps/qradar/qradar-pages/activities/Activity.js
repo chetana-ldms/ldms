c7f6a34976590e9eec46c4c0d45ba49c387d165a
NaveenaEB <Naveena.EB@lancesoft.in>
2024-04-08 17:39:33 +0530

    new changes

--- src/app/modules/apps/qradar/qradar-pages/activities/Activity.js
+++ src/app/modules/apps/qradar/qradar-pages/activities/Activity.js
@@ -1,8 +1,311 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {
+  fetchActivitiesUrl,
+  fetchActivityTypesUrl,
+  fetchSetOfActivity,
+} from '../../../../../api/ActivityApi'
+import {UsersListLoading} from '../components/loading/UsersListLoading'
+import {getCurrentTimeZone} from '../../../../../../utils/helper'
+import Pagination from '../../../../../../utils/Pagination'
+import ReactPaginate from 'react-paginate'
+import {fetchUsersUrl} from '../../../../../api/ConfigurationApi'
+import Select from 'react-select'
 
 function Activity() {
+  const [activity, setActivity] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
+  const [selectedUsers, setSelectedUsers] = useState([])
+  const [selectedActivityTypes, setSelectedActivityTypes] = useState([]) // State for selected activity types
+  const [activityType, setActivityType] = useState([])
+  const [userEmail, setUserEmail] = useState('')
+  const [selectedFromDate, setSelectedFromDate] = useState(null)
+  const [selectedToDate, setSelectedToDate] = useState(null)
+  const orgId = Number(sessionStorage.getItem('orgId'))
+  const userID = Number(sessionStorage.getItem('userId'))
+  const [limit, setLimit] = useState(20)
+  const [pageCount, setPageCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const reload = async () => {
+    try {
+      setLoading(true)
+      const data = await fetchUsersUrl(orgId)
+      setUsers(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    reload()
+  }, [])
+
+  const reloadActivityType = async () => {
+    try {
+      setLoading(true)
+      const data = await fetchActivityTypesUrl(null)
+      setActivityType(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    reloadActivityType()
+  }, [])
+
+  const fetchData = async () => {
+
+    const data = {
+      orgId: orgId,
+      userId: 0,
+      activityTypeIds: [],
+      fromDateTime: selectedFromDate ? selectedFromDate.toISOString() : null,
+      toDateTime: selectedToDate ? selectedToDate.toISOString() : null,
+      paging: {
+        rangeStart: 1,
+        rangeEnd: limit,
+      },
+    }
+
+    try {
+      setLoading(true)
+      const response = await fetchActivitiesUrl(data)
+      setActivity(response.activitiesList)
+      const total = response.totalActivitiesCount
+      setPageCount(Math.ceil(total / limit))
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [selectedUsers, selectedFromDate, selectedToDate, selectedActivityTypes])
+
+  const handlePageSelect = (event) => {
+    const selectedPerPage = event.target.value
+    setLimit(selectedPerPage)
+  }
+
+  const handlePageClick = async (data) => {
+    let currentPage = data.selected + 1
+    setLoading(true)
+    const setOfAlertsData = await fetchSetOfActivity(currentPage, orgId, userID, limit)
+    setActivity(setOfAlertsData)
+    setCurrentPage(currentPage)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [limit])
+
+  const handleFromDateChange = (date) => {
+    setSelectedFromDate(date)
+  }
+
+  const handleToDateChange = (date) => {
+    setSelectedToDate(date)
+  }
+
+  const handleUserChange = (selectedOptions) => {
+    setSelectedUsers(selectedOptions)
+  }
+
+  const handleActivityTypeChange = (selectedOptions) => {
+    setSelectedActivityTypes(selectedOptions) // Update selected activity types state
+  }
+
+  const userOptions = users.map((user) => ({label: user.name, value: user}))
+  const activityTypeOptions = activityType.map((type) => ({label: type.typeName, value: type}))
+  const css_classes = [
+    'text-primary',
+    'text-secondary',
+    'text-success',
+    'text-danger',
+    'text-warning',
+    'text-info',
+    'text-dark',
+    'text-muted',
+  ]
+
+  const getRandomClass = () => {
+    const randomIndex = Math.floor(Math.random() * css_classes.length)
+    return css_classes[randomIndex]
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Selected Users:', selectedUsers);
+    console.log('Selected Activity Types:', selectedActivityTypes);
+    console.log('Selected From Date:', selectedFromDate);
+    console.log('Selected To Date:', selectedToDate);
+        const selectedUserIDs = selectedUsers.map((user) => user.value.userID)
+        console.log(selectedUserIDs, "selectedUserIDs")
+        const selectedActivityTypeIDs = selectedActivityTypes.map(
+          (activityType) => activityType.value.id
+        )
+    
+        const data = {
+          orgId: orgId,
+          userId: selectedUsers || 0,
+          activityTypeIds: selectedActivityTypes || [],
+          fromDateTime: selectedFromDate ? selectedFromDate.toISOString() : null,
+          toDateTime: selectedToDate ? selectedToDate.toISOString() : null,
+          paging: {
+            rangeStart: 1,
+            rangeEnd: limit,
+          },
+        }
+    
+        try {
+          setLoading(true)
+          const response = await fetchActivitiesUrl(data)
+          setActivity(response.activitiesList)
+          const total = response.totalActivitiesCount
+          setPageCount(Math.ceil(total / limit))
+        } catch (error) {
+          console.error(error)
+        } finally {
+          setLoading(false)
+        }
+      
+  };
+
   return (
-    <div>Activity</div>
+    <div>
+      {loading ? (
+        <UsersListLoading />
+      ) : (
+        <>
+           <>
+          <div className='d-flex'>
+            <div className='mb-3 d-flex align-items-center'>
+              <label className='me-2'>Users:</label>
+              <Select
+                options={userOptions}
+                isMulti={false}
+                value={selectedUsers}
+                onChange={handleUserChange}
+                placeholder='Select Users'
+              />
+            </div>
+
+            <div className='mb-3 d-flex align-items-center'>
+              <label className='me-2'>Activity Types:</label>
+              <Select
+                options={activityTypeOptions}
+                isMulti
+                value={selectedActivityTypes}
+                onChange={handleActivityTypeChange}
+                placeholder='Select Activity Types'
+              />
+            </div>
+
+            <div className='mb-3'>
+              <label>From Date: </label>
+              <input
+                type='date'
+                value={
+                  selectedFromDate
+                    ? selectedFromDate.toISOString().split('T')[0]
+                    : ''
+                }
+                onChange={(e) => handleFromDateChange(new Date(e.target.value))}
+              />
+            </div>
+            <div className='mb-3'>
+              <label>To Date: </label>
+              <input
+                type='date'
+                value={
+                  selectedToDate
+                    ? selectedToDate.toISOString().split('T')[0]
+                    : ''
+                }
+                onChange={(e) => handleToDateChange(new Date(e.target.value))}
+              />
+            </div>
+            
+          <button className='btn btn-primary ms-10' onClick={handleSubmit}>Submit</button>
+          </div>
+        </>
+
+          <div className='timeline-label'>
+            {activity !== null && activity!==undefined ? (
+              activity
+                // .sort((a, b) => b.activityId - a.activityId)
+                .map((item) => {
+                  const formattedDateTime = getCurrentTimeZone(item.activityDate)
+
+                  return (
+                    <div className='timeline-item' key={item.id}>
+                      <div className='timeline-label fw-bold text-gray-800 fs-6'>
+                        <p className='semi-bold'>{formattedDateTime}</p>
+                        <p className='text-muted normal'>{item.createedUser}</p>
+                      </div>
+
+                      <div className='timeline-badge'>
+                        <i className={`fa fa-genderless ${getRandomClass()} fs-1`}></i>
+                      </div>
+                      <div className='fw-semibold text-gray-700 ps-3 fs-7'>
+                        {item.primaryDescription}
+                      </div>
+                    </div>
+                  )
+                })
+            ) : (
+              <div className='text-gray-500 text-center'>No data found</div>
+            )}
+          </div>
+
+          <div className='d-flex justify-content-end align-items-center pagination-bar mt-5'>
+            <ReactPaginate
+              previousLabel=<i className='fa fa-chevron-left' />
+              nextLabel=<i className='fa fa-chevron-right' />
+              pageCount={pageCount}
+              marginPagesDisplayed={1}
+              pageRangeDisplayed={8}
+              onPageChange={handlePageClick}
+              containerClassName={'pagination justify-content-end'}
+              pageClassName={'page-item'}
+              pageLinkClassName={'page-link'}
+              previousClassName={'page-item custom-previous'}
+              previousLinkClassName={'page-link custom-previous-link'}
+              nextClassName={'page-item custom-next'}
+              nextLinkClassName={'page-link custom-next-link'}
+              breakClassName={'page-item'}
+              breakLinkClassName={'page-link'}
+              activeClassName={'active'}
+            />
+            <div className='col-md-3 d-flex justify-content-end align-items-center'>
+              <span className='col-md-4'>Count: </span>
+              <select
+                className='form-select form-select-sm col-md-4'
+                value={limit}
+                onChange={handlePageSelect}
+              >
+                <option value={5}>5</option>
+                <option value={10}>10</option>
+                <option value={15}>15</option>
+                <option value={20}>20</option>
+              </select>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
 
