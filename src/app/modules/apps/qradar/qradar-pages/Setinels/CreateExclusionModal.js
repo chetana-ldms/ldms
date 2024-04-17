199a91229fb87737730a84b32dd4fd05fd4997ad
NaveenaEB <Naveena.EB@lancesoft.in>
2024-04-17 18:07:18 +0530

    new changes

--- src/app/modules/apps/qradar/qradar-pages/Setinels/CreateExclusionModal.js
+++ src/app/modules/apps/qradar/qradar-pages/Setinels/CreateExclusionModal.js
@@ -18,9 +18,20 @@ const CreateExclusionModal = ({show, onClose, refreshParent}) => {
   const [selectedValue, setSelectedValue] = useState('Hash')
   const [includeSubfolders, setIncludeSubfolders] = useState(false)
   const [changeEnabled, setChangeEnabled] = useState(false)
+  const [excludeAlerts, setExcludeAlerts] = useState(true)
+  const [excludeBinaryVault, setExcludeBinaryVault] = useState(false)
+  const [error, setError] = useState(false)
+
   const handleSubmit = async () => {
     try {
+      if (!excludeAlerts && !excludeBinaryVault) {
+        setError(true)
+        notifyFail('You must select one or more Exclusion Function')
+        return
+      }
+
       if (!osDropdownRef.current.value || !sha1InputRef.current.value) {
+        setError(false)
         notifyFail('Please fill out all mandatory fields.')
         return
       }
@@ -40,6 +51,7 @@ const CreateExclusionModal = ({show, onClose, refreshParent}) => {
         mode: 'string',
         type: 'string',
       }
+
       const responseData = await fetchAddToExclusionListUrl(data)
       const {isSuccess, message} = responseData
 
@@ -54,13 +66,16 @@ const CreateExclusionModal = ({show, onClose, refreshParent}) => {
       console.error('Error during API call:', error)
     }
   }
+
   const handleDropdownToggle = () => {
     setDropdownOpen(!dropdownOpen)
   }
+
   const handleDropdownItemClick = (value) => {
     setSelectedValue(value)
     setDropdownOpen(false)
   }
+
   const handleToggleChange = () => {
     if (changeEnabled) {
       setIncludeSubfolders(!includeSubfolders)
@@ -69,6 +84,16 @@ const CreateExclusionModal = ({show, onClose, refreshParent}) => {
 
   const handleToggleEnabled = () => {
     setChangeEnabled(!changeEnabled)
+  }
+
+  const handleCheckboxChange = (e, checkboxType) => {
+    const isChecked = e.target.checked
+    if (checkboxType === 'excludeAlerts') {
+      setExcludeAlerts(isChecked)
+    } else if (checkboxType === 'excludeBinaryVault') {
+      setExcludeBinaryVault(isChecked)
+    }
+    setError(!(isChecked || excludeBinaryVault))
   }
 
   return (
@@ -114,69 +139,167 @@ const CreateExclusionModal = ({show, onClose, refreshParent}) => {
             </select>
           </div>
         </div>
-        <div className='row mt-5'>
-          <div className='col-md-9'>
-            <div>
-              <label className='form-label' htmlFor='sha1Input'>
-                SHA1*
-              </label>
-              <input type='text' className='form-control' ref={sha1InputRef} required />
-            </div>
-          </div>
-          <div className='col-md-3  text-primary d-flex align-items-end justify-content-end pb-3 '>
-            <i className='bi bi-search text-primary mr-2'></i> Threat
-          </div>
-        </div>
-        <div className='row mt-5'>
-          <div className='col-md-9'>
-            <div>
-              <label className='form-label' htmlFor='sha1Input'>
-                Path*
-              </label>
-              <input
-                type='text'
-                className='form-control'
-                placeholder='Example: /bin/file or /bin/'
-                ref={sha1InputRef}
-                required
-              />
-            </div>
-            <div>
-              <div className=''>
-                <span>As File</span>&nbsp;
-                <span onClick={handleToggleEnabled} style={{cursor: 'pointer'}}>
-                  Change
-                </span>
-              </div>
+        {selectedValue == 'Hash' && (
+          <div className='row mt-5'>
+            <div className='col-md-9'>
               <div>
+                <label className='form-label' htmlFor='sha1Input'>
+                  SHA1*
+                </label>
+                <input type='text' className='form-control' ref={sha1InputRef} required />
+              </div>
+            </div>
+            <div className='col-md-3  text-primary d-flex align-items-end justify-content-end pb-3 '>
+              <i className='bi bi-search text-primary mr-2'></i> Threat
+            </div>
+          </div>
+        )}
+        {selectedValue == 'Path' && (
+          <div className='row mt-5'>
+            <div className='col-md-9'>
+              <div>
+                <label className='form-label' htmlFor='sha1Input'>
+                  Path*
+                </label>
                 <input
-                  type='checkbox'
-                  checked={includeSubfolders}
-                  onChange={handleToggleChange}
-                  disabled={!changeEnabled}
+                  type='text'
+                  className='form-control'
+                  placeholder='Example: /bin/file or /bin/'
+                  ref={sha1InputRef}
+                  required
                 />
-                <label>Include Subfolders</label>
               </div>
-            </div>
-            <hr />
-            <div>
-              <p>Exclusion Function</p>
+              <div className='col-md-3  text-primary d-flex align-items-end justify-content-end pb-3 '>
+                <i className='bi bi-search text-primary mr-2'></i> Threat
+              </div>
               <div>
-                <input type='checkbox' />
-                <label>Exclude path for alerts and mitigation</label>
+                <div className=''>
+                  <span>As File</span>&nbsp;
+                  <span onClick={handleToggleEnabled} style={{cursor: 'pointer'}}>
+                    Change
+                  </span>
+                </div>
+                <div>
+                  <input
+                    type='checkbox'
+                    checked={includeSubfolders}
+                    onChange={handleToggleChange}
+                    disabled={!changeEnabled}
+                  />
+                  <label>Include Subfolders</label>
+                </div>
               </div>
-              <p>
-                The selected Exclusion Mode (Suppress Alerts, Interoperability, or Performance
-                Focus) applies to files in the path
-              </p>
+              <hr />
               <div>
-                <input type='checkbox' />
-                <label>Exclude path for Binary Vault</label>
+                <p>Exclusion Function</p>
+                <div>
+                  <input
+                    type='checkbox'
+                    checked={excludeAlerts}
+                    onChange={(e) => handleCheckboxChange(e, 'excludeAlerts')}
+                  />
+                  <label>Exclude path for alerts and mitigation</label>
+                </div>
+                <p>
+                  The selected Exclusion Mode (Suppress Alerts, Interoperability, or Performance
+                  Focus) applies to files in the path
+                </p>
+                <div>
+                  <input
+                    type='checkbox'
+                    checked={excludeBinaryVault}
+                    onChange={(e) => handleCheckboxChange(e, 'excludeBinaryVault')}
+                  />
+                  <label>Exclude path for Binary Vault</label>
+                </div>
+                <p>Files in the path are not uploaded automatically</p>
+                {error && (
+                  <p style={{color: 'red'}}>You must select one or more Exclusion Function</p>
+                )}
               </div>
-              <p>Files in the path are not uploaded automatically</p>
             </div>
           </div>
-        </div>
+        )}
+        {selectedValue == 'Certificate' && (
+          <div className='row mt-5'>
+            <div className='col-md-9'>
+              <div>
+                <label className='form-label' htmlFor='sha1Input'>
+                  Signer Identity*
+                </label>
+                <input type='text' className='form-control' ref={sha1InputRef} required />
+              </div>
+              <div className='col-md-3  text-primary d-flex align-items-end justify-content-end pb-3 '>
+                <i className='bi bi-search text-primary mr-2'></i> Threat
+              </div>
+              <div>
+                <h5>About Signer Identity:</h5>
+                <p>1. Go to the Incidents page and select the relevant threat</p>
+                <p>2. Copy the Signer Identity value</p>
+                <p>3. Paste in the Signer Identity field in the exclusion</p>
+              </div>
+            </div>
+          </div>
+        )}
+        {selectedValue == 'File Type' && (
+          <div className='row mt-5'>
+            <div className='col-md-9'>
+              <div>
+                <label className='form-label' htmlFor='sha1Input'>
+                  File Type
+                </label>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='Example: txt'
+                  ref={sha1InputRef}
+                  required
+                />
+              </div>
+              <div className='col-md-3  text-primary d-flex align-items-end justify-content-end pb-3 '>
+                <i className='bi bi-search text-primary mr-2'></i> Threat
+              </div>
+              <div>
+                <p>Exclusion Function</p>
+                <div>
+                  <input
+                    type='checkbox'
+                    checked={excludeAlerts}
+                    onChange={(e) => handleCheckboxChange(e, 'excludeAlerts')}
+                  />
+                  <label>Exclude file type for alerts and mitigation</label>
+                </div>
+                <p>Alerts and mitigation are suppressed for files of this type</p>
+                <div>
+                  <input
+                    type='checkbox'
+                    checked={excludeBinaryVault}
+                    onChange={(e) => handleCheckboxChange(e, 'excludeBinaryVault')}
+                  />
+                  <label>Exclude file type for Binary Vault</label>
+                </div>
+                <p>Files of this type are not uploaded automatically</p>
+                {error && (
+                  <p style={{color: 'red'}}>You must select one or more Exclusion Function</p>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+        {selectedValue == 'Browser' && (
+          <div className='row mt-5'>
+            <label htmlFor='browserInput' className='form-label'>
+            BROWSER*
+            </label>
+            <select className='form-select' id='browserInput' ref={osDropdownRef} required>
+              <option value=''>Select</option>
+              <option value='firefox'>Firefox</option>
+              <option value='edge'>Edge</option>
+              <option value='internet explorer'>Internet Explorer</option>
+              <option value='chrome'>Chrome</option>
+            </select>
+          </div>
+        )}
         <div className='mt-5'>
           <label className='form-label'>Description</label>
           <textarea
