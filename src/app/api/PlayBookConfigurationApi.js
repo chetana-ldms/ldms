27912140e1798fd0fce07a3828d2a767e59676da
NaveenaEB <Naveena.EB@lancesoft.in>
2026-05-20 15:12:26 +0530

    new changes

--- src/app/api/PlayBookConfigurationApi.js
+++ src/app/api/PlayBookConfigurationApi.js
@@ -1,6 +1,7 @@
 import FetchWithToken from "../modules/auth/FetchWithToken"
 const ResolverSearchUrl = 'http://10.41.3.232:501/api/ResolverEngine/v1/Resolver/Search'
 const ResolverDeleteUrl = 'http://10.41.3.232:501/api/ResolverEngine/v1/Resolver/Delete'
+const ResolverAddUrl = 'http://10.41.3.232:501/api/ResolverEngine/v1/Resolver/Add'
 
 export const fetchResolverSearchUrl = async (data) => {
   try {
@@ -24,6 +25,26 @@ export const fetchResolverSearchUrl = async (data) => {
 export const fetchResolverDeleteUrl = async (data) => {
   try {
     const response = await FetchWithToken(`${ResolverDeleteUrl}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...data,
+      }),
+    })
+
+    const responseData = await response.json()
+    console.log(responseData, 'responseData111')
+    return responseData
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const fetchResolverAddUrl = async (data) => {
+  try {
+    const response = await FetchWithToken(`${ResolverAddUrl}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
